5a45548b570d8f2d504271a2eec8c6c127fa8729
levisingularity <rafael.levi@singularitynet.io>
2023-10-28 19:24:42 -0300

    fix: payload access action and input

--- handler.py
+++ handler.py
@@ -31,140 +31,140 @@ def handle(event: str, context=None):
     result = None
     actions = Actions()
 
-    if payload.action == ActionType.NODE_EXISTS:
+    if payload["action"] == ActionType.NODE_EXISTS:
         node_exists_payload = validate(
             NodeExistsValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.node_exists(
             node_exists_payload.node_type,
             node_exists_payload.node_name,
         )
-    elif payload.action == ActionType.LINK_EXISTS:
+    elif payload["action"] == ActionType.LINK_EXISTS:
         link_exists_payload = validate(
             LinkExistsValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.link_exists(
             link_exists_payload.link_type,
             link_exists_payload.target_handles,
         )
-    elif payload.action == ActionType.GET_NODE_HANDLE:
+    elif payload["action"] == ActionType.GET_NODE_HANDLE:
         get_node_handle_payload = validate(
             GetNodeHandleValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_node_handle(
             get_node_handle_payload.node_type,
             get_node_handle_payload.node_name,
         )
-    elif payload.action == ActionType.GET_LINK_HANDLE:
+    elif payload["action"] == ActionType.GET_LINK_HANDLE:
         get_link_handle_payload = validate(GetLinkHandleValidator())
         result = actions.get_link_handle(
             get_link_handle_payload.link_type,
             get_link_handle_payload.target_handles,
         )
-    elif payload.action == ActionType.GET_LINK_TARGETS:
+    elif payload["action"] == ActionType.GET_LINK_TARGETS:
         get_link_targets_payload = validate(
             GetLinkTargetsValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_link_targets(get_link_targets_payload.link_handle)
-    elif payload.action == ActionType.IS_ORDERED:
+    elif payload["action"] == ActionType.IS_ORDERED:
         is_ordered_payload = validate(
             IsOrderedValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.is_ordered(is_ordered_payload.link_handle)
-    elif payload.action == ActionType.GET_MATCHED_LINKS:
+    elif payload["action"] == ActionType.GET_MATCHED_LINKS:
         get_matched_links_payload = validate(
             GetMatchedLinksValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_matched_links(
             get_matched_links_payload.link_type,
             get_matched_links_payload.target_handles,
             get_matched_links_payload.extra_parameters,
         )
-    elif payload.action == ActionType.GET_ALL_NODES:
+    elif payload["action"] == ActionType.GET_ALL_NODES:
         get_all_nodes_payload = validate(
             GetAllNodeValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_all_nodes(
             get_all_nodes_payload.link_type,
             get_all_nodes_payload.names,
         )
-    elif payload.action == ActionType.GET_MATCHED_TYPE_TEMPLATE:
+    elif payload["action"] == ActionType.GET_MATCHED_TYPE_TEMPLATE:
         get_matched_type_template_payload = validate(
             GetMatchedTypeTemplateValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_matched_type_template(
             get_matched_type_template_payload.template,
             get_matched_type_template_payload.extra_parameters,
         )
-    elif payload.action == ActionType.GET_MATCHED_TYPE:
+    elif payload["action"] == ActionType.GET_MATCHED_TYPE:
         get_matched_type_payload = validate(
             GetMatchedTypeValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_matched_type(
             get_matched_type_payload.link_type,
             get_matched_type_payload.extra_parameters,
         )
-    elif payload.action == ActionType.GET_NODE_NAME:
+    elif payload["action"] == ActionType.GET_NODE_NAME:
         get_node_name_payload = validate(
             GetNodeNameValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_node_name(get_node_name_payload.node_handle)
-    elif payload.action == ActionType.GET_MATCHED_NODE_NAME:
+    elif payload["action"] == ActionType.GET_MATCHED_NODE_NAME:
         get_matched_node_name_payload = validate(
             GetMatchedNodeNameValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_matched_node_name(
             get_matched_node_name_payload.node_type,
             get_matched_node_name_payload.substring,
         )
-    elif payload.action == ActionType.GET_ATOM_AS_DICT:
+    elif payload["action"] == ActionType.GET_ATOM_AS_DICT:
         get_atom_as_dict_payload = validate(
             GetAtomAsDictValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_atom_as_dict(
             get_atom_as_dict_payload.handle,
             get_atom_as_dict_payload.arity,
         )
-    elif payload.action == ActionType.GET_ATOM_AS_DEEP_REPRESENTATION:
+    elif payload["action"] == ActionType.GET_ATOM_AS_DEEP_REPRESENTATION:
         get_atom_as_deep_representation_payload = (
             actions.get_atom_as_deep_representation(
                 GetAtomAsDeepRepresentationValidator(),
-                payload.input,
+                payload["input"],
             )
         )
         result = actions.get_atom_as_deep_representation(
             get_atom_as_deep_representation_payload.handle,
             get_atom_as_deep_representation_payload.arity,
         )
-    elif payload.action == ActionType.GET_LINK_TYPE:
+    elif payload["action"] == ActionType.GET_LINK_TYPE:
         get_link_type_payload = validate(
             GetLinkTypeValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_link_type(get_link_type_payload.link_handle)
-    elif payload.action == ActionType.GET_NODE_TYPE:
+    elif payload["action"] == ActionType.GET_NODE_TYPE:
         get_node_type_payload = validate(
             GetNodeTypeValidator(),
-            payload.input,
+            payload["input"],
         )
         result = actions.get_node_type(get_node_type_payload.node_handle)
-    elif payload.action == ActionType.COUNT_ATOMS:
+    elif payload["action"] == ActionType.COUNT_ATOMS:
         result = actions.count_atoms()
-    elif payload.action == ActionType.CLEAR_DATABASE:
+    elif payload["action"] == ActionType.CLEAR_DATABASE:
         result = actions.clear_database()
-    elif payload.action == ActionType.PING:
+    elif payload["action"] == ActionType.PING:
         result = {"message": "pong"}
     else:
         raise UnknownEventAction()
