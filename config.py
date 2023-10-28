34b7b09e05e52354ea28f7b80ce6b3a2f0de4057
levisingularity <rafael.levi@singularitynet.io>
2023-10-28 14:36:08 -0300

    fix: environments key

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 
 def load_env():
     environments = {
-        "das_mongodb_name": "DAS_MONGODB_NAME",
-        "das_mongodb_port": "DAS_MONGODB_PORT",
-        "das_mongodb_hostname": "DAS_MONGODB_HOSTNAME",
-        "das_mongodb_username": "DAS_MONGODB_USERNAME",
-        "das_mongodb_password": "DAS_MONGODB_PASSWORD",
-        "das_redis_hostname": "DAS_REDIS_HOSTNAME",
-        "das_redis_port": "DAS_REDIS_PORT",
+        "das-mongodb-name": "DAS_MONGODB_NAME",
+        "das-mongodb-port": "DAS_MONGODB_PORT",
+        "das-mongodb-hostname": "DAS_MONGODB_HOSTNAME",
+        "das-mongodb-username": "DAS_MONGODB_USERNAME",
+        "das-mongodb-password": "DAS_MONGODB_PASSWORD",
+        "das-redis-hostname": "DAS_REDIS_HOSTNAME",
+        "das-redis-port": "DAS_REDIS_PORT",
     }
 
     for key, value in environments.items():
@@ -19,5 +19,3 @@ def load_env():
                 os.environ[value] = f.readline()
         else:
             os.environ[value] = os.environ[key]
-
-    
