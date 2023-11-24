f0ff4d398657299fffabf5dde93c84d9cb8f1c10
Levi <141682181+levisingularity@users.noreply.github.com>
2023-11-24 15:33:24 -0300

    fix: validation error (#21)

--- das-query-engine/handler.py
+++ das-query-engine/handler.py
@@ -61,149 +61,110 @@ def handle(event: any, context=None):
         http_code_response = 400
         return _response(http_code_response, result, context)
 
-    if payload["action"] == ActionType.PING:
-        result = dict(message="pong")
-    elif payload["action"] == ActionType.CLEAR_DATABASE:
-        result = actions.clear_database()
-    elif payload["action"] == ActionType.COUNT_ATOMS:
-        result = actions.count_atoms()
-    elif payload["action"] == ActionType.GET_ATOM:
-        get_atom_payload = validate(
-            GetAtomValidator(),
-            payload["input"],
-        )
-        try:
+    try:
+        if payload["action"] == ActionType.PING:
+            result = dict(message="pong")
+        elif payload["action"] == ActionType.CLEAR_DATABASE:
+            result = actions.clear_database()
+        elif payload["action"] == ActionType.COUNT_ATOMS:
+            result = actions.count_atoms()
+        elif payload["action"] == ActionType.GET_ATOM:
+            get_atom_payload = validate(
+                GetAtomValidator(),
+                payload["input"],
+            )
             result = actions.get_atom(
                 handle=get_atom_payload["handle"],
                 output_format=get_atom_payload.get("output_format", None),
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_NODE:
-        get_node_payload = validate(
-            GetNodeValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.GET_NODE:
+            get_node_payload = validate(
+                GetNodeValidator(),
+                payload["input"],
+            )
             result = actions.get_node(
                 node_type=get_node_payload["node_type"],
                 node_name=get_node_payload["node_name"],
                 output_format=get_node_payload.get("output_format", None),
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_NODES:
-        get_nodes_payload = validate(
-            GetNodesValidator(),
-            payload["input"],
-        )
-        try:
-            result = actions.get_nodes(
-                node_type=get_nodes_payload["node_type"],
-                node_name=get_nodes_payload["node_name"],
-                output_format=get_nodes_payload.get("output_format", None),
+
+        elif payload["action"] == ActionType.GET_NODES:
+                get_nodes_payload = validate(
+                    GetNodesValidator(),
+                    payload["input"],
+                )
+                result = actions.get_nodes(
+                    node_type=get_nodes_payload["node_type"],
+                    node_name=get_nodes_payload["node_name"],
+                    output_format=get_nodes_payload.get("output_format", None),
+                )
+        elif payload["action"] == ActionType.GET_LINK:
+            get_link_payload = validate(
+                GetLinkValidator(),
+                payload["input"],
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_LINK:
-        get_link_payload = validate(
-            GetLinkValidator(),
-            payload["input"],
-        )
-        try:
             result = actions.get_link(
                 link_type=get_link_payload["link_type"],
                 targets=get_link_payload["targets"],
                 output_format=get_link_payload.get("output_format", None),
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_LINKS:
-        get_links_payload = validate(
-            GetLinksValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.GET_LINKS:
+            get_links_payload = validate(
+                GetLinksValidator(),
+                payload["input"],
+            )
             result = actions.get_links(
                 link_type=get_links_payload["link_type"],
                 target_types=get_links_payload.get("target_types", None),
                 targets=get_links_payload.get("targets", None),
                 output_format=get_links_payload.get("output_format", None),
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_LINK_TYPE:
-        get_link_type_payload = validate(
-            GetLinkTypeValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.GET_LINK_TYPE:
+            get_link_type_payload = validate(
+                GetLinkTypeValidator(),
+                payload["input"],
+            )
             result = actions.get_link_type(
                 link_handle=get_link_type_payload["link_handle"],
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_LINK_TARGETS:
-        get_link_targets_payload = validate(
-            GetLinkTargetsValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.GET_LINK_TARGETS:
+            get_link_targets_payload = validate(
+                GetLinkTargetsValidator(),
+                payload["input"],
+            )
             result = actions.get_link_targets(
                 link_handle=get_link_targets_payload["link_handle"],
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_NODE_TYPE:
-        get_node_type_payload = validate(
-            GetNodeTypeValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.GET_NODE_TYPE:
+            get_node_type_payload = validate(
+                GetNodeTypeValidator(),
+                payload["input"],
+            )
             result = actions.get_node_type(
                 node_handle=get_node_type_payload["node_handle"],
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.GET_NODE_NAME:
-        get_node_name_payload = validate(
-            GetNodeNameValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.GET_NODE_NAME:
+            get_node_name_payload = validate(
+                GetNodeNameValidator(),
+                payload["input"],
+            )
             result = actions.get_node_name(
                 node_handle=get_node_name_payload["node_handle"],
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.QUERY:
-        query_payload = validate(
-            QueryValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.QUERY:
+            query_payload = validate(
+                QueryValidator(),
+                payload["input"],
+            )
             result = actions.query(
                 query=query_payload["query"],
                 extra_parameters=query_payload.get("extra_parameters", None),
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
-    elif payload["action"] == ActionType.PATTERN_MATCHER_QUERY:
-        pattern_matcher_query_payload = validate(
-            PatternMatcherQueryValidator(),
-            payload["input"],
-        )
-        try:
+        elif payload["action"] == ActionType.PATTERN_MATCHER_QUERY:
+            pattern_matcher_query_payload = validate(
+                PatternMatcherQueryValidator(),
+                payload["input"],
+            )
             logical_expression_parser = LogicalExpressionParser()
             query = logical_expression_parser.from_dict(
                 pattern_matcher_query_payload["query"]
@@ -217,8 +178,8 @@ def handle(event: any, context=None):
                     "extra_parameters", None
                 ),
             )
-        except Exception as e:
-            result = dict(error=str(e))
-            http_code_response = 500
+    except Exception as e:
+        result = dict(error=str(e))
+        http_code_response = 500
 
     return _response(http_code_response, result, context)
