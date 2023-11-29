3fec027b64bda2f0c706359b5d6920f9b4a16dd4
levisingularity <rafael.levi@singularitynet.io>
2023-11-29 14:04:58 -0300

    refactor: move env schema to another file

---
+++ das-query-engine/env.py
@@ -0,0 +1,77 @@
+env_schema = {
+    "dasmongodbname": {
+        "key": "DAS_MONGODB_NAME",
+        "hidden": False,
+        "required": True,
+    },
+    "dasmongodbport": {
+        "key": "DAS_MONGODB_PORT",
+        "hidden": False,
+        "required": True,
+    },
+    "dasmongodbhostname": {
+        "key": "DAS_MONGODB_HOSTNAME",
+        "hidden": False,
+        "required": True,
+    },
+    "dasmongodbusername": {
+        "key": "DAS_MONGODB_USERNAME",
+        "hidden": True,
+        "required": True,
+    },
+    "dasmongodbpassword": {
+        "key": "DAS_MONGODB_PASSWORD",
+        "hidden": True,
+        "required": True,
+    },
+    "dasredishostname": {
+        "key": "DAS_REDIS_HOSTNAME",
+        "hidden": False,
+        "required": True,
+    },
+    "dasredisport": {
+        "key": "DAS_REDIS_PORT",
+        "hidden": False,
+        "required": True,
+    },
+    "dasredispassword": {
+        "key": "DAS_REDIS_PASSWORD",
+        "hidden": True,
+        "required": False,
+    },
+    "dasredisusername": {
+        "key": "DAS_REDIS_USERNAME",
+        "hidden": True,
+        "required": False,
+    },
+    "dasuseredisssl": {
+        "key": "DAS_USE_REDIS_SSL",
+        "hidden": False,
+        "required": False,
+    },
+    "dasmongodbtlscafile": {
+        "key": "DAS_MONGODB_TLS_CA_FILE",
+        "hidden": False,
+        "required": False,
+    },
+    "dasuserediscluster": {
+        "key": "DAS_USE_REDIS_CLUSTER",
+        "hidden": False,
+        "required": False,
+    },
+    "dasusecachednodes": {
+        "key": "DAS_USE_CACHED_NODES",
+        "hidden": False,
+        "required": False,
+    },
+    "dasusecachedlinktypes": {
+        "key": "DAS_USE_CACHED_LINK_TYPES",
+        "hidden": False,
+        "required": False,
+    },
+    "dasusecachednodetypes": {
+        "key": "DAS_USE_CACHED_NODE_TYPES",
+        "hidden": False,
+        "required": False,
+    },
+}
