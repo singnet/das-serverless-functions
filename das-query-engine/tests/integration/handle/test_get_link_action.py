6d5ea6a4a29dfda3a54b37036fe17231ddfd687a
levisingularity <rafael.levi@singularitynet.io>
2023-12-22 17:37:08 -0300

    tests: handler actions

---
+++ das-query-engine/tests/integration/handle/test_get_link_action.py
@@ -0,0 +1,63 @@
+from actions import ActionType
+from tests.integration.handle.base_test_action import BaseTestHandlerAction
+import pytest
+
+
+class TestGetLinkAction(BaseTestHandlerAction):
+    @pytest.fixture
+    def action_type(self):
+        return ActionType.GET_LINK
+
+    @pytest.fixture
+    def valid_event(self, action_type):
+        return {
+            "body": {
+                "action": action_type,
+                "input": {
+                    "link_type": "Inheritance",
+                    "link_targets": [
+                        "4e8e26e3276af8a5c2ac2cc2dc95c6d2",
+                        "80aff30094874e75028033a38ce677bb",
+                    ],
+                },
+            }
+        }
+
+    @pytest.fixture
+    def expected_output(self):
+        return {
+            "handle": "ee1c03e6d1f104ccd811cfbba018451a",
+            "composite_type_hash": "41c082428b28d7e9ea96160f7fd614ad",
+            "is_toplevel": True,
+            "composite_type": [
+                "e40489cd1e7102e35469c937e05c8bba",
+                "d99a604c79ce3c2e76a2f43488d5d4c3",
+                "d99a604c79ce3c2e76a2f43488d5d4c3",
+            ],
+            "named_type": "Inheritance",
+            "named_type_hash": "e40489cd1e7102e35469c937e05c8bba",
+            "targets": [
+                "4e8e26e3276af8a5c2ac2cc2dc95c6d2",
+                "80aff30094874e75028033a38ce677bb",
+            ],
+        }
+
+    def test_get_link_action(
+        self,
+        valid_event,
+        expected_output,
+    ):
+        self.assert_successful_execution(valid_event, expected_output)
+
+    def test_malformed_payload(self, malformed_event):
+        self.assert_payload_malformed(malformed_event)
+
+    def test_unknown_action(self, unknown_action_event):
+        self.assert_unknown_action_dispatcher(unknown_action_event)
+
+    def test_unexpected_exception(
+        self,
+        mocker,
+        valid_event,
+    ):
+        self.assert_unexpected_exception(mocker, valid_event)
---
+++ das-query-engine/tests/integration/handle/test_get_node_action.py
@@ -0,0 +1,47 @@
+from actions import ActionType
+from tests.integration.handle.base_test_action import BaseTestHandlerAction
+import pytest
+
+
+class TestGetNodeAction(BaseTestHandlerAction):
+    @pytest.fixture
+    def action_type(self):
+        return ActionType.GET_NODE
+
+    @pytest.fixture
+    def valid_event(self, action_type):
+        return {
+            "body": {
+                "action": action_type,
+                "input": {"node_type": "Concept", "node_name": "human"},
+            }
+        }
+
+    @pytest.fixture
+    def expected_output(self):
+        return {
+            "handle": "af12f10f9ae2002a1607ba0b47ba8407",
+            "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
+            "name": "human",
+            "named_type": "Concept",
+        }
+
+    def test_get_node_action(
+        self,
+        valid_event,
+        expected_output,
+    ):
+        self.assert_successful_execution(valid_event, expected_output)
+
+    def test_malformed_payload(self, malformed_event):
+        self.assert_payload_malformed(malformed_event)
+
+    def test_unknown_action(self, unknown_action_event):
+        self.assert_unknown_action_dispatcher(unknown_action_event)
+
+    def test_unexpected_exception(
+        self,
+        mocker,
+        valid_event,
+    ):
+        self.assert_unexpected_exception(mocker, valid_event)
