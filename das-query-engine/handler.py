57516be772dc2c14b0fd0aae19fcf21f263108c9
levisingularity <rafael.levi@singularitynet.io>
2024-03-20 18:48:27 -0300

    Fix serializing/deserializing query answers

--- das-query-engine/handler.py
+++ das-query-engine/handler.py
@@ -1,5 +1,3 @@
-import base64
-import json
 import traceback
 
 from action_dispatcher import ActionDispatcher
@@ -9,50 +7,33 @@ from hyperon_das.logger import logger
 from utils.dotenv import load_env
 from validators import validate
 from validators.event import EventValidator
+from typing import Any
+
 
 load_env()
 
-
 def _response(
     http_code_response: int,
-    result: str,
+    result: Any,
     headers: dict = {},
 ):
     logger().info(f"Function status code response - {http_code_response}")
 
     return {
         "statusCode": http_code_response,
-        "body": json.dumps(result, default=str, indent=4),
-        "headers": {
-            "Content-Type": "application/json",
-            "X-Handler-Method-Timestamp": headers.get("X-Handler-Method-Timestamp", None),
-        },
+        "body": result,
+        "headers": headers,
     }
 
 
-def _get_payload(event: any):
-    body = event.get("body", event)
-
-    if isinstance(body, str):  # aws
-        if event.get("isBase64Encoded") is True:
-            logger().info(f"Received AWS source payload (Base64 encoded): {body}")
-            return json.loads(base64.b64decode(body))
-
-        logger().info(f"Received AWS source payload: {body}")
-        return json.loads(body)
-
-    logger().info(f"Received Vultr payload source: {event}")
-    return body  # vultr
-
-
 # TODO: refactor status code comes from actions now and the handler only returns it to the client
-def handle(event: any, context=None):
+def handle(event: Any, context=None):
     result = None
     http_code_response = None
     elapsed_time = None
 
     try:
-        payload = validate(EventValidator(), _get_payload(event))
+        payload = validate(EventValidator(), event.get("body", {}))
 
         action_dispatcher = ActionDispatcher(action_mapper=ActionMapper())
 
