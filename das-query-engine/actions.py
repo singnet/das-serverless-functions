31845e363f9344533427f2550fdd998a8f4e34e3
marcocapozzoli <marcocapozzoli90@gmail.com>
2024-03-27 18:11:11 -0300

    add system_parameters

--- das-query-engine/actions.py
+++ das-query-engine/actions.py
@@ -33,6 +33,7 @@ class Actions:
         try:
             self.distributed_atom_space = DistributedAtomSpace(
                 atomdb="redis_mongo",
+                system_parameters={'running_on_server': True},
                 mongo_hostname=os.getenv("DAS_MONGODB_HOSTNAME"),
                 mongo_port=int(os.getenv("DAS_MONGODB_PORT")),
                 mongo_username=os.getenv("DAS_MONGODB_USERNAME"),
