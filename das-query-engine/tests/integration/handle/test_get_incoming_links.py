41147cb78159d1f2774cf52c4a6f8e81a2b502ca
levisingularity <rafael.levi@singularitynet.io>
2024-04-12 21:21:14 -0300

    Add get incoming links

--- das-query-engine/tests/integration/handle/test_get_incoming_links.py
+++ das-query-engine/tests/integration/handle/test_get_incoming_links.py
@@ -1,9 +1,12 @@
 import pytest
 from actions import ActionType
-from tests.integration.handle.base_test_action import BaseTestHandlerAction
+from tests.integration.handle.base_test_action import BaseTestHandlerAction, symbol, human
+from hyperon_das_atomdb.utils.expression_hasher import ExpressionHasher
 
 
 class TestGetIncomingLinksAction(BaseTestHandlerAction):
+    human_handle = ExpressionHasher.terminal_hash(symbol, human)
+
     @pytest.fixture
     def action_type(self):
         return ActionType.GET_INCOMING_LINKS
@@ -14,270 +17,59 @@ class TestGetIncomingLinksAction(BaseTestHandlerAction):
             "body": {
                 "action": action_type,
                 "input": {
-                    "atom_handle": "af12f10f9ae2002a1607ba0b47ba8407",
+                    "atom_handle": self.human_handle,
                     "kwargs": {
                         "targets_document": True,
+                        "no_iterator": True
                     },
                 },
             }
         }
 
-    @pytest.fixture
-    def expected_output(self):
-        return [
-            [
-                {
-                    "handle": "16f7e407087bfa0b35b13d13a1aadcae",
-                    "composite_type_hash": "ed73ea081d170e1d89fc950820ce1cee",
-                    "is_toplevel": True,
-                    "composite_type": [
-                        "a9dea78180588431ec64d6bc4872fdbc",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                    ],
-                    "named_type": "Similarity",
-                    "named_type_hash": "a9dea78180588431ec64d6bc4872fdbc",
-                    "targets": [
-                        "af12f10f9ae2002a1607ba0b47ba8407",
-                        "4e8e26e3276af8a5c2ac2cc2dc95c6d2",
-                    ],
-                    "type": "Similarity",
-                },
-                [
-                    {
-                        "handle": "af12f10f9ae2002a1607ba0b47ba8407",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "human",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                    {
-                        "handle": "4e8e26e3276af8a5c2ac2cc2dc95c6d2",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "ent",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                ],
-            ],
-            [
-                {
-                    "handle": "a45af31b43ee5ea271214338a5a5bd61",
-                    "composite_type_hash": "ed73ea081d170e1d89fc950820ce1cee",
-                    "is_toplevel": True,
-                    "composite_type": [
-                        "a9dea78180588431ec64d6bc4872fdbc",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                    ],
-                    "named_type": "Similarity",
-                    "named_type_hash": "a9dea78180588431ec64d6bc4872fdbc",
-                    "targets": [
-                        "4e8e26e3276af8a5c2ac2cc2dc95c6d2",
-                        "af12f10f9ae2002a1607ba0b47ba8407",
-                    ],
-                    "type": "Similarity",
-                },
-                [
-                    {
-                        "handle": "4e8e26e3276af8a5c2ac2cc2dc95c6d2",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "ent",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                    {
-                        "handle": "af12f10f9ae2002a1607ba0b47ba8407",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "human",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                ],
-            ],
-            [
-                {
-                    "handle": "2c927fdc6c0f1272ee439ceb76a6d1a4",
-                    "composite_type_hash": "ed73ea081d170e1d89fc950820ce1cee",
-                    "is_toplevel": True,
-                    "composite_type": [
-                        "a9dea78180588431ec64d6bc4872fdbc",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                    ],
-                    "named_type": "Similarity",
-                    "named_type_hash": "a9dea78180588431ec64d6bc4872fdbc",
-                    "targets": [
-                        "5b34c54bee150c04f9fa584b899dc030",
-                        "af12f10f9ae2002a1607ba0b47ba8407",
-                    ],
-                    "type": "Similarity",
-                },
-                [
-                    {
-                        "handle": "5b34c54bee150c04f9fa584b899dc030",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "chimp",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                    {
-                        "handle": "af12f10f9ae2002a1607ba0b47ba8407",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "human",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                ],
-            ],
-            [
-                {
-                    "handle": "b5459e299a5c5e8662c427f7e01b3bf1",
-                    "composite_type_hash": "ed73ea081d170e1d89fc950820ce1cee",
-                    "is_toplevel": True,
-                    "composite_type": [
-                        "a9dea78180588431ec64d6bc4872fdbc",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                    ],
-                    "named_type": "Similarity",
-                    "named_type_hash": "a9dea78180588431ec64d6bc4872fdbc",
-                    "targets": [
-                        "af12f10f9ae2002a1607ba0b47ba8407",
-                        "5b34c54bee150c04f9fa584b899dc030",
-                    ],
-                    "type": "Similarity",
-                },
-                [
-                    {
-                        "handle": "af12f10f9ae2002a1607ba0b47ba8407",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "human",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                    {
-                        "handle": "5b34c54bee150c04f9fa584b899dc030",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "chimp",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                ],
-            ],
-            [
-                {
-                    "handle": "c93e1e758c53912638438e2a7d7f7b7f",
-                    "composite_type_hash": "41c082428b28d7e9ea96160f7fd614ad",
-                    "is_toplevel": True,
-                    "composite_type": [
-                        "e40489cd1e7102e35469c937e05c8bba",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                    ],
-                    "named_type": "Inheritance",
-                    "named_type_hash": "e40489cd1e7102e35469c937e05c8bba",
-                    "targets": [
-                        "af12f10f9ae2002a1607ba0b47ba8407",
-                        "bdfe4e7a431f73386f37c6448afe5840",
-                    ],
-                    "type": "Inheritance",
-                },
-                [
-                    {
-                        "handle": "af12f10f9ae2002a1607ba0b47ba8407",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "human",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                    {
-                        "handle": "bdfe4e7a431f73386f37c6448afe5840",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "mammal",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                ],
-            ],
-            [
-                {
-                    "handle": "2a8a69c01305563932b957de4b3a9ba6",
-                    "composite_type_hash": "ed73ea081d170e1d89fc950820ce1cee",
-                    "is_toplevel": True,
-                    "composite_type": [
-                        "a9dea78180588431ec64d6bc4872fdbc",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                    ],
-                    "named_type": "Similarity",
-                    "named_type_hash": "a9dea78180588431ec64d6bc4872fdbc",
-                    "targets": [
-                        "1cdffc6b0b89ff41d68bec237481d1e1",
-                        "af12f10f9ae2002a1607ba0b47ba8407",
-                    ],
-                    "type": "Similarity",
-                },
-                [
-                    {
-                        "handle": "1cdffc6b0b89ff41d68bec237481d1e1",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "monkey",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                    {
-                        "handle": "af12f10f9ae2002a1607ba0b47ba8407",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "human",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                ],
-            ],
-            [
-                {
-                    "handle": "bad7472f41a0e7d601ca294eb4607c3a",
-                    "composite_type_hash": "ed73ea081d170e1d89fc950820ce1cee",
-                    "is_toplevel": True,
-                    "composite_type": [
-                        "a9dea78180588431ec64d6bc4872fdbc",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "d99a604c79ce3c2e76a2f43488d5d4c3",
-                    ],
-                    "named_type": "Similarity",
-                    "named_type_hash": "a9dea78180588431ec64d6bc4872fdbc",
-                    "targets": [
-                        "af12f10f9ae2002a1607ba0b47ba8407",
-                        "1cdffc6b0b89ff41d68bec237481d1e1",
-                    ],
-                    "type": "Similarity",
-                },
-                [
-                    {
-                        "handle": "af12f10f9ae2002a1607ba0b47ba8407",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "human",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                    {
-                        "handle": "1cdffc6b0b89ff41d68bec237481d1e1",
-                        "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
-                        "name": "monkey",
-                        "named_type": "Concept",
-                        "type": "Concept",
-                    },
-                ],
-            ],
-        ]
 
     def test_incoming_links_action(
         self,
         valid_event,
-        expected_output,
     ):
-        self.assert_successful_execution(valid_event, expected_output)
+        body, status_code = self.make_request(valid_event)
+        expected_status_code = 200
+
+        assert status_code == expected_status_code, f"Unexpected status code: {status_code}. Expected: {expected_status_code}"
+        assert isinstance(body, list)
+        assert len(body) > 0
+        
+        for item in body:
+            assert isinstance(item, tuple)
+            assert len(item) == 2
+
+            item_dict, item_tuple = item
+
+            assert isinstance(item_dict, dict)
+
+            assert isinstance(item_dict.get("handle"), str)
+            assert isinstance(item_dict.get("type"), str)
+            assert isinstance(item_dict.get("composite_type_hash"), str)
+            assert isinstance(item_dict.get("is_toplevel"), bool)
+            assert isinstance(item_dict.get("composite_type"), list)
+            assert all((isinstance(item, str)) for item in item_dict.get("composite_type"))
+
+            assert isinstance(item_dict.get("named_type"), str)
+            assert isinstance(item_dict.get("named_type_hash"), str)
+            assert isinstance(item_dict.get("targets"), list)
+            assert all((isinstance(item, str)) for item in item_dict.get("targets"))
+
+            assert isinstance(item_tuple, list)
+
+            for atom in item_tuple:
+                assert isinstance(atom, dict)
+                assert isinstance(atom.get("handle"), str)
+                assert isinstance(atom.get("type"), str)
+                assert isinstance(atom.get("composite_type_hash"), str)
+                assert isinstance(atom.get("name"), str)
+                assert isinstance(atom.get("named_type"), str)
+                assert isinstance(atom.get("is_literal"), bool)
+
+
 
     def test_malformed_payload(self, malformed_event):
         self.assert_payload_malformed(malformed_event)
